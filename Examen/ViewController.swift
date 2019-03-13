8fb912fdb1585aa930a01baf1c7ce40b982cea02
Alex Lin <alexlinsegarra@gmail.com>
2019-03-13 18:33:56 +0100

    Initial Commit

---
+++ Examen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examen
+//
+//  Created by Alex Lin on 13/03/2019.
+//  Copyright © 2019 Alex Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
